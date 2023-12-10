93455c63bac15750842c7ac84630607d96c7f222
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-12-10 18:39:11 +0530

    fff

--- app/page.js
+++ app/page.js
@@ -14,7 +14,9 @@ const page = () => {
   <input type='text' placeholder='Enter title' className='border-2 p-3 m-3 text-1xl border-black' value={title} onChange={(e) => {
     settitle(e.target.value)
   }}/>
-  
+  <input type='text' placeholder='Enter description' className='border-2 p-3 m-3 text-1xl border-black' value={desc} onChange={(e) => {
+    setdesc(e.target.value)
+  }}/>
   <button className='bg-black text-white px-5 py-3 border-2 hover:cursor-pointer rounded'>Add_Task</button>
 </form>
 
